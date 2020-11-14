b1e2f7b7362381e5a48ec3fba060ccc2420befcd
Pavel Pelovski <herrpelovski@gmail.com>
2020-11-14 23:25:32 +0100

    Implement Data Models - News, Category and Image

---
+++ Data/ArsenalFanPage.Data.Models/Image.cs
@@ -0,0 +1,24 @@
+﻿namespace ArsenalFanPage.Data.Models
+{
+    using System;
+
+    using ArsenalFanPage.Data.Common.Models;
+
+    public class Image : BaseDeletableModel<string>
+    {
+        public Image()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public int NewsId { get; set; }
+
+        public virtual News News { get; set; }
+
+        public string CreatedByUserId { get; set; }
+
+        public virtual ApplicationUser CreatedByUser { get; set; }
+
+        public string Extension { get; set; }
+    }
+}
---
+++ Data/ArsenalFanPage.Data.Models/News.cs
@@ -0,0 +1,30 @@
+﻿namespace ArsenalFanPage.Data.Models
+{
+    using System.Collections.Generic;
+
+    using ArsenalFanPage.Data.Common.Models;
+
+    public class News : BaseDeletableModel<int>
+    {
+        public News()
+        {
+            this.Images = new HashSet<Image>();
+        }
+
+        public string GeneralTitle { get; set; }
+
+        public string DescriptionTitle { get; set; }
+
+        public string Description { get; set; }
+
+        public string CreatedByUserId { get; set; }
+
+        public virtual ApplicationUser CreatedByUser { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+    }
+}
