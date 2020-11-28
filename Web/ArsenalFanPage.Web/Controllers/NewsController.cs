c490e20c6e3fd4161736cdf221ad0ae3f2752bc7
Pavel Pelovski <herrpelovski@gmail.com>
2020-11-28 21:21:39 +0100

    Create News View and News Controller

---
+++ Web/ArsenalFanPage.Web/Controllers/NewsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArsenalFanPage.Web.Controllers
+{
+    public class NewsController : BaseController
+    {
+        [HttpGet("/News")]
+        public IActionResult News()
+        {
+            return this.View();
+        }
+    }
+}
