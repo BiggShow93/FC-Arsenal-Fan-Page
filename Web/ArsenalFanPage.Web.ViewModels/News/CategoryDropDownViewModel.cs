4d077e7e1a84f1b7e887e4dfae6f589e0f8da37f
Pavel Pelovski <herrpelovski@gmail.com>
2020-11-29 18:16:04 +0100

    Implement ICategoriesService, CategoriesService and CategoryDropDownViewModel

---
+++ Web/ArsenalFanPage.Web.ViewModels/News/CategoryDropDownViewModel.cs
@@ -0,0 +1,12 @@
+﻿using ArsenalFanPage.Data.Models;
+using ArsenalFanPage.Services.Mapping;
+
+namespace ArsenalFanPage.Web.ViewModels.News
+{
+    public class CategoryDropDownViewModel : IMapFrom<Category>
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- Web/ArsenalFanPage.Web.ViewModels/News/NewsCreateInputModel.cs
+++ Web/ArsenalFanPage.Web.ViewModels/News/NewsCreateInputModel.cs
@@ -14,6 +14,7 @@
         public string Content { get; set; }
 
         [Required]
+        [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
         // TODO: Upload Image
